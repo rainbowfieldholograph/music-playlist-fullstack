abc50842e109181c30ea35430f0c2cb8ddc4c254
cherry <somebleed@tutanota.com>
2022-04-30 16:45:34 +0900

    updated to React 18 / some other packages update / little styles fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import client from './graphql/apollo';
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
